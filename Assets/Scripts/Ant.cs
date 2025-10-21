7a54a23e3280020e92074616e48c30084b4c5318
dunkizz <dunkizs2589@gmail.com>
2025-10-21 22:41:38 +0700

    Lab 9

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,12 +2,45 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
-    // Start is called once before the first execution of Update 
-    void Start()
-    {
-    }
-    public override void Behavior()
-    {
-        throw new System.NotImplementedException();
-    }
+    public Vector2 velocity;
+    public Transform[] movePoints;
+    private Rigidbody2D rb;
+     void Start()
+     {
+     base.Initialize(20);
+     DamageHit = 20;
+     //set speed and direction of movement
+     velocity = new Vector2(-1.0f, 0.0f); //start with moving left
+     rb = GetComponent<Rigidbody2D>();
+     }
+     
+     public override void Behavior()
+     {
+     //move from current position
+     rb.MovePosition( rb.position + velocity * Time.fixedDeltaTime );
+     //move left และเกินขอบซ้าย
+     if ( velocity.x < 0 && rb.position.x <= movePoints[0].position.x )
+     {
+     Flip();
+     }
+     //move right และเกินขอบขวา
+     if ( velocity.x > 0 && rb.position.x >= movePoints[1].position.x )
+     {
+     Flip();
+     }
+     }
+     //flip ant to the opposite direction
+     public void Flip()
+     {
+     velocity.x *= -1; //change direction of movement
+     //Flip the image
+     Vector3 theScale = transform.localScale;
+     theScale.x *= -1;
+     transform.localScale = theScale;
+     }
+     private void FixedUpdate()
+     {
+     Behavior();
+     }
+     
 }
